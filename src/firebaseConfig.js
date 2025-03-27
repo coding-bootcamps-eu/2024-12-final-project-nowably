3a324ad7bfcfdaf3e50140eb16fb16245d21676a
Franzi <kiel.franziska@outlook.com>
2025-03-27 18:22:00 +0100

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,12 +5,6 @@ import { getAuth } from 'firebase/auth' // Authentication
 import { getFirestore } from 'firebase/firestore' // Firestore
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDrMu57LasVNJS4rTaNH64LsU9L6_BZZ4k',
-  authDomain: 'nowably-7967d.firebaseapp.com',
-  projectId: 'nowably-7967d',
-  storageBucket: 'nowably-7967d.firebasestorage.app',
-  messagingSenderId: '539993233584',
-  appId: '1:539993233584:web:91cd28739dcb890f130375',
 }
 
 // Initialize Firebase
